485c1e7316534a56914fc101862c2876a7f2c3a7
Vladyslav Kozyr <vladyslav.kozyr@gmail.com>
2024-04-03 23:08:57 +0300

    Add empty functions

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def input_text():
+    pass
+
+
+def read_file(path: str):
+    pass
+
+
+def read_file_pandas(path: str):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def output_console(text):
+    pass
+
+
+def write_file(path, content):
+    pass
