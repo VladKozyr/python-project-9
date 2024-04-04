b494627bfe6ebf60317405c514d4711f088ce8b7
Vladyslav Kozyr <vladyslav.kozyr@gmail.com>
2024-04-04 10:02:25 +0300

    Add docstrings

--- app/io/input.py
+++ app/io/input.py
@@ -1,10 +1,43 @@
-def input_text():
+import pandas as pd
+
+
+def input_text() -> str:
+    """
+    Function to input text from the console.
+
+    Returns:
+        str: The text entered by the user from the console.
+    """
     pass
 
 
-def read_file(path: str):
+def read_file(path: str) -> str:
+    """
+    Function to read content from a file using built-in Python capabilities.
+
+    Args:
+        path (str): File path
+
+    Returns:
+        str: The content read from the file.
+
+    Raises:
+        FileNotFoundError - If the file does not exist
+    """
     pass
 
 
-def read_file_pandas(path: str):
+def read_file_pandas(path: str) -> pd.DataFrame:
+    """
+    Function to read content from a file using the pandas library.
+
+    Args:
+        path (str): File path
+
+    Returns:
+        pandas.DataFrame: The content read from the file as a DataFrame.
+
+    Raises:
+        FileNotFoundError - If the file does not exist
+    """
     pass
--- app/io/output.py
+++ app/io/output.py
@@ -1,6 +1,28 @@
-def output_console(text):
+def output_console(text: str):
+    """
+    Function to print text to the console.
+
+    Args:
+        text (str): The text to be printed to the console.
+
+    Returns:
+        None
+    """
     pass
 
 
-def write_file(path, content):
+def write_file(path: str, content: str):
+    """
+    Function to write content to a file.
+
+    Args:
+        path (str): The path to the file to write
+        content (str): The content to write to the file
+
+    Returns:
+        None
+
+    Raises:
+        IOError - If the file does not exist or some IO error happen
+    """
     pass
